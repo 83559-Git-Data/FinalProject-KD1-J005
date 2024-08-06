1b1a24f14532bf4f19f36bb7927af1258e232059
Your Name <anupamvaishy2001@gmail.com.com>
2024-08-06 14:30:09 +0530

    Client-Registration Screen completed By Anupam Vaishy

--- ScheduleMasterFrontend/src/components/navbar.jsx
+++ ScheduleMasterFrontend/src/components/navbar.jsx
@@ -1,73 +1,63 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
+import "./Navbar.css"; // Import the custom CSS file
+import logo from "../assets/images/schedulemaster.webp";
 
-function Navbar() {
+function Navbar({ toggleSidebar }) {
   return (
-    <nav
-      className='navbar navbar-expand-lg bg-body-tertiary bg-dark'
-      data-bs-theme='dark'
-    >
-      <div className='container-fluid'>
-        <a className='navbar-brand' href='#'>
-          MyAirbnb
+    <nav className="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#" onClick={toggleSidebar}>
+          <img src={logo} alt="Logo" className="navbar-logo" />
+          Schedule Master
         </a>
 
-        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-          <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
-            <li className='nav-item'>
-              <Link
-                to='/home'
-                className='nav-link'
-                aria-current='page'
-                href='#'
-              >
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          onClick={toggleSidebar}>
+          ☰
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link to="/home" className="nav-link">
                 Home
               </Link>
             </li>
-            <li className='nav-item'>
-              <Link
-                to='/student'
-                className='nav-link'
-                aria-current='page'
-                href='#'
-              >
+            <li className="nav-item">
+              <Link to="/student" className="nav-link">
                 Student
               </Link>
             </li>
-            <li className='nav-item'>
-              <Link
-                to='/users'
-                className='nav-link'
-                aria-current='page'
-                href='#'
-              >
-                Users
+
+            <li className="nav-item">
+              <Link to="/aboutus" className="nav-link">
+                About Us
               </Link>
             </li>
-            <li className='nav-item'>
-              <Link
-                to='/bookings'
-                className='nav-link'
-                aria-current='page'
-                href='#'
-              >
-                Bookings
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link
-                to='/login'
-                className='nav-link'
-                aria-current='page'
-                href='#'
-              >
+            <li className="nav-item">
+              <Link to="/login" className="nav-link">
                 Logout
               </Link>
             </li>
           </ul>
+          <div className="d-flex align-items-center">
+            <img
+              src="/path-to-your-profile-pic.png"
+              alt="Profile"
+              className="profile-pic"
+            />
+            <button className="btn btn-outline-secondary ms-2">Profile</button>
+          </div>
         </div>
       </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;

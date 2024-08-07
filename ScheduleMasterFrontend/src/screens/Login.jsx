70df1a5e221e65ac5a73f4dfd00e586e87981d25
Your Name <anupamvaishy2001@gmail.com.com>
2024-08-07 11:15:13 +0530

    Add Course Component By Anupam Vaishy

--- ScheduleMasterFrontend/src/screens/Login.jsx
+++ ScheduleMasterFrontend/src/screens/Login.jsx
@@ -1,105 +1,70 @@
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { login } from "../services/admin";
-import bg from "../Images/a-well-lit-classroom-setting-with-a-central-focus--9iaZJ3P5TlaLHgUpr_gT-w-YB-loG-_R3Op7sgSsvSmEQ.jpeg";
+import "./Common.css";
 
-function Login(){
-    const [userName, setuserName] = useState('')
-    const [password, setPassword] = useState('')
-    const [isuserNameEmpty, setuserNameEmpty] = useState(false)
-    const [isPasswordEmpty, setPasswordEmpty] = useState(false)
-    //conditional rendering --> line 20
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    // get navigation hook
-    const navigate = useNavigate()
-
-    const onLogin = () => {
-        if(userName.length == 0)
-        {
-            toast.error('Please enter email');
-        }
-        else if(password.length == 0)
-        {
-            toast.error('Please enter password');
-        }
-        else{
-            // call login API and check its success
-            // go to home screen
-            // const result = await login(userName, password)
-            // if(result['status'] == 'success')
-            //     {
-            //         const data = result['data']
-            //         sessionStorage['name'] = data['name']
-            //         sessionStorage['token'] = data['token']
-                    navigate('/home')
-                // }else{
-                //     toast.error(result['error'])
-                // }
-        }
+  // get navigation hook
+  const navigate = useNavigate();
+  const onLogin = async () => {
+    if (email.length == 0) {
+      toast.error("Please enter email");
+    } else if (password.length == 0) {
+      toast.error("Please enter password");
+    } else {
+      navigate("/home");
     }
+  };
 
-
-    return(
-        <div style={{
-            backgroundImage: `url(${bg})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            height: "100vh",
-            width: "100vw",
-          }}>
-            
-            <h2 className="page-header">Login</h2>
-            <div className="row">
-            <div className='col'></div>
-            <div className='col'>
-                <div className='form'>
-                    <div className="mb-3">
-                        <label htmlFor="">Login</label>
-                        <input 
-                        onChange={(e) => {
-                        if(e.target.value.length == 0){
-                            setuserNameEmpty(true)
-                        } else{
-                            setuserNameEmpty(false)
-                        }
-                        setuserName(e.target.value)
-                        }}    
-                        type="userName" className="form-control"/>
-                        {isuserNameEmpty && (
-                        <p style={{color: 'red'}}>userName is manadatory</p>
-                    )}
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="">Password</label>
-                        <input 
-                        onChange={(e) => {
-                        if(e.target.value.length == 0){
-                            setPasswordEmpty(true)
-                        }else {
-                            setPasswordEmpty(false)
-                        }
-                            setPassword(e.target.value)
-                        }}
-                        type="password" className="form-control"/>
-                        {isPasswordEmpty && (
-                        <p style={{color: 'red'}}>Password is manadatory</p>
-                    )}
-                    </div>
-                
-                    <div className="mb-3">
-                        <div>
-                            Don't have an account ? <Link to='/register'>Register Here</Link>
-                        </div>
-                    <button onClick={onLogin} className="btn btn-success mt-2">Login</button>
-                    </div>
-                </div>  
+  return (
+    <div className="login-container">
+      <div className="row">
+        <div className="col"></div>
+        <div className="col ">
+          <div className="login-box shadow p-4">
+            <h2 style={{ alignContent: "center" }}>Login</h2>
+            <div className="form">
+              <div className="mb-3">
+                <label htmlFor="">Email</label>
+                <input
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                  type="email"
+                  className="form-control"
+                  placeholder="Enter your email"
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="">Password</label>
+                <input
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                  type="password"
+                  className="form-control"
+                  placeholder="Enter your Password"
+                />
+              </div>
+              <div className="mb-3">
+                <div>
+                  Don't have account ? <Link to="/register">Register here</Link>
+                </div>
+                <button onClick={onLogin} className="btn btn-success mt-2">
+                  Login
+                </button>
+              </div>
             </div>
-            
-            <div className='col'></div>
-            </div>
+          </div>
         </div>
-    )
+        <div className="col"></div>
+      </div>
+    </div>
+  );
 }
 
-export default Login; 
+export default Login;

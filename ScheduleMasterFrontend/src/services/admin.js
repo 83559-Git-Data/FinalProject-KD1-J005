8c7a32b07e004d9ddbe09649fbdec9680ecb79f2
Your Name <anupamvaishy2001@gmail.com.com>
2024-08-07 15:39:47 +0530

    Add Service for registration and in Registration.jsx Integeration is Completed

--- ScheduleMasterFrontend/src/services/admin.js
+++ ScheduleMasterFrontend/src/services/admin.js
@@ -1,42 +1,21 @@
-import axios from 'axios'
-import { config } from './config'
+import axios from "axios";
+import { config } from "./config";
 
 export async function register(firstName, lastName, userName, password, role) {
   debugger;
   try {
     // post body
-    const body = { firstName, lastName, userName, password, role }
+    const body = { firstName, lastName, userName, password, role };
 
     // send the post request
-    const response = await axios.post(`${config.serverUrl}/user/register`, body)
+    const response = await axios.post(
+      `${config.serverUrl}/user/register`,
+      body
+    );
 
     // return the json body from response object
-    return response.data
+    return response;
   } catch (ex) {
-    console.log(`exception: `, ex)
-    return null
+    console.log(`exception: `, ex);
   }
-
-  
-}
-
-export async function login(userName, password) {
-  const body = { userName, password }
-  try {
-    const response = await axios.post(`${config.serverUrl}/user/login`, body)
-    return response.data
-  } catch (ex) {
-    console.log(`exception: `, ex)
-  }
-
-  return null
-  //   axios
-  //     .post(`${config.serverUrl}/admin/login`, body)
-  //     .then((response) => {
-  //       console.log(response.data)
-  //       return response.data
-  //     })
-  //     .catch((error) => {
-  //       console.log(error)
-  //     })
 }
